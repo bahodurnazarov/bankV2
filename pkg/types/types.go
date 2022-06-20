5c1b21ac23f7d364aab750f357b2b478a202962d
Nazarov Bahodur <nazarovbahodur1999@gmail.com>
2022-06-20 11:13:17 +0500

    hw from ex 10 to 12

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -7,23 +7,23 @@ type Category string
 type Status string
 
 const (
-	StatusOk Status = "OK"
-	StatusFail Status = "FAIL"
+	StatusOk         Status = "OK"
+	StatusFail       Status = "FAIL"
 	StatusInProgress Status = "INPROGRESS"
 )
 
 type Payment struct {
-	ID int64
+	ID        int64
 	AccountID int64
-	Amount Money
-	Category Category
-	Status Status
+	Amount    Money
+	Category  Category
+	Status    Status
 }
 
 type Phone string
 
 type Account struct {
-	ID int64
-	Phone Phone
+	ID      int64
+	Phone   Phone
 	Balance Money
 }
